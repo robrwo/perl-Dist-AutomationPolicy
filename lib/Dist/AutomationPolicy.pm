8d367b4ab985449a43755544f0336b6bc6cbcf6f
Robert Rothenberg <perl@rhizomnic.com>
2026-05-15 17:36:38 +0100

    Commit Changes and bump $VERSION

--- lib/Dist/AutomationPolicy.pm
+++ lib/Dist/AutomationPolicy.pm
@@ -16,7 +16,7 @@ use experimental qw( postderef signatures );
 
 use namespace::autoclean;
 
-our $VERSION = 'v0.2.0';
+our $VERSION = 'v0.2.1';
 
 # ABSTRACT: generate and parse distribution automation policies
 
